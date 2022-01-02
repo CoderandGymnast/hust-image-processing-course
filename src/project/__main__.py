26e7ca3e738d5c38e3ee35fb8f8804e07a6832f5
Hung.VS <HungICT2407@gmail.com>
2022-01-02 13:28:19 +0700

    [project]: init

---
+++ src/project/__main__.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+PATH_BASE="."
+PATH_DATA="./data"
+
+img=cv2.cvtColor(cv2.imread(f"{PATH_DATA}/1_wIXlvBeAFtNVgJd49VObgQ.png_Salt_Pepper_Noise1.png", cv2.IMREAD_GRAYSCALE),cv2.COLOR_BGR2RGB)
+
+imgs=[img]
+
+for ksize in range(3,12,2):
+    imgs.append(cv2.cvtColor(cv2.medianBlur(img,ksize), cv2.COLOR_BGR2RGB))
+
+plt.figure(figsize=(5,5))
+for i in range(6):
+	ax=plt.subplot(3,2,i+1)	
+	plt.imshow(imgs[i])
+	plt.title(i*2+1)
+	plt.axis("off")
+ 
+plt.show()
+
+"""
+selected ksize: 7
+"""
+
